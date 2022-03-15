9d804dd842700f3cb0fe5746572b4b91b6f72af2
gustavosimon <gustavo.andre.simon@hotmail.com>
2022-03-15 20:39:41 -0300

    first commit

---
+++ src/pdi/PDI.java
@@ -0,0 +1,17 @@
+package pdi;
+
+/**
+ * Trabalho de Processamento Digital de Imagens
+ * @author Gustavo Simon e Felipe Wozniak
+ */
+public class PDI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu m = new Menu();
+        m.setVisible(true);
+    }
+    
+}
